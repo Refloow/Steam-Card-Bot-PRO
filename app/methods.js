d8d76aca034a19608b346edd9ff216d5f5f2cd58
Refloow <59449926+Refloow@users.noreply.github.com>
2020-03-20 09:45:38 +0100

    Adding methods for future updates.

---
+++ app/methods.js
@@ -0,0 +1,26 @@
+// We strongly recommend not editing stuff that is in this file.
+
+const package = require('./../package.json');
+const config = require('./SETTINGS/config.js');
+const colors = require('colors');
+const moment = require('moment');
+
+t = module.exports = {
+    
+    check: function() {
+        const request = require('request');
+        var options = {
+            url: 'https://raw.githubusercontent.com/Refloow/Steam-Card-Bot-PRO/master/package.json',
+            method: 'GET',
+        };
+        function look(error, JSONresponse, body) {
+            var page = JSON.parse(body)
+            const v = package.version;
+            if(page.version != v)
+                console.log(`| [GitHub] | VERSION |:  ${'New update available for '+package.name+ ' v'+page.version.green+'! You\'re currently only running version '+v.yellow+''}\n${`| [GitHub] | VERSION |: Go to https://github.com/Refloow/Steam-Card-Bot-PRO to update now!`}\n\n`)
+            else 
+                console.log(`| [GitHub] | VERSION |: You're running the latest version of Steam-Card-Bot-PRO (v${v.green})\n\n`)
+        }
+        request(options, look)
+    }
+}
