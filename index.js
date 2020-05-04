2317a4afa39d142d5af30000282617010d37078a
Refloow <59449926+Refloow@users.noreply.github.com>
2020-05-04 07:17:16 +0200

    Updated to 1.3.0

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
+// Steam-Card-Bot-PRO built by Refloow (-MajokingGames)
+
+/* 
+  Here is contact info: refloowlibrarycontact@gmail.com
+  or main dev steam: https://steamcommunity.com/id/MajokingGames/
+
+*/
+
+// Checking if all modules are correctly installed
+
+try {
+    // Checking if module colors is correctly installed
+  colors = require('colors');
+} catch (ex) {
+  // If modules are not installed showing an clear error message to user.
+  console.log('| [Modules] |: Missing dependencies. Install a version with dependecies or use npm install.');
+  console.log(ex);
+  process.exit(1);
+}
+
+// Starting app file
+
 require('./app/app.js');
-const colors = require('colors'); // Requires colors.
+
 
 // Basic display information on app start
 
@@ -12,9 +34,18 @@ console.log('888 T88b  88888888888   888888  888888  888888  888  888'.cyan);
 console.log('888  T88b Y8b.    888   888Y88..88PY88..88PY88b 888 d88P '.cyan);
 console.log('888   T88b "Y8888 888   888 "Y88P"  "Y88P"  "Y8888888P" \n\n\n'.cyan);                              
 console.log('This bot was developed by MajokingGames'.cyan);
-console.log('Verision 1.2.1 PRO'.cyan);
+console.log('Verision 1.3.0 PRO'.cyan);
 console.log('Thanks for choosing us.'.cyan);
 console.log('This version doesnt contain UI. Login not required...'.red);
 console.log('Loading config file...'.green );
 console.log('Starting LevelUp bot...'.green );
 console.log('If bot doesnt start in next 1 minute steam is down.\n\n'.green );
+
+// Steam-Card-Bot-PRO built by Refloow (-MajokingGames)
+
+/* 
+  Here is contact info: refloowlibrarycontact@gmail.com
+  or main dev steam: https://steamcommunity.com/id/MajokingGames/
+
+*/
+
