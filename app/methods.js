f6e1526e1a3b9931f3f7b023691b3922f54e18b8
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2020-09-14 23:58:51 +0200

    Updated methods to 1.5.0

--- app/methods.js
+++ app/methods.js
@@ -39,13 +39,27 @@ t = module.exports = {
             var page = JSON.parse(body)
             const v = package.version;
             if(page.version != v)
-                console.log(` [GitHub] | VERSION |:  ${'New update available for '+package.name+ ' v'+page.version.green+'! You\'re currently only running version '+v.yellow+''}\n${` [GitHub] | VERSION |: Go to https://github.com/Refloow/Steam-Card-Bot-PRO to update now!`}\n\n`)
+                console.log(`\n [GitHub] | VERSION |:  ${'New update available for '+package.name+ ' v'+page.version.green+'! You\'re currently only running version '+v.yellow+''}\n${` [GitHub] | VERSION |: Go to https://github.com/Refloow/Steam-Card-Bot-PRO to update now!`}\n\n`)
             else 
                 console.log(` [GitHub] | VERSION |: You're running the latest version of Steam-Card-Bot-PRO (v${v.green})\n\n`)
         }
         request(options, look)
     },
 
+    checkaswego: function() {
+        var options = {
+            url: 'https://raw.githubusercontent.com/Refloow/Steam-Card-Bot-PRO/master/package.json',
+            method: 'GET',
+        };
+        function look(error, JSONresponse, body) {
+            var page = JSON.parse(body)
+            const v = package.version;
+            if(page.version != v)
+                console.log(`\n [GitHub] | VERSION |:  ${'New update available for '+package.name+ ' v'+page.version.green+'! You\'re currently only running version '+v.yellow+''}\n${` [GitHub] | VERSION |: Go to https://github.com/Refloow/Steam-Card-Bot-PRO to update now!`}\n\n`)
+        }
+        request(options, look)
+    },
+ 
     // This method checks if any of this values are not set in the config
     validatelogininfo: function() {
         
@@ -111,6 +125,10 @@ t = module.exports = {
     },
 
     // Other bot functions
+
+    LogsCalledCommandsEnabled: function() {
+        return config.CalledCommandsLive == true;
+    },
 
     ChatSpamProtectionEnabled: function() {
         return config.chat_spam_protection == true;
