d0e9b95b9d7964116deefb511bbd225cf3a96a24
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2021-01-03 00:32:17 +0100

    Added note

--- app/methods.js
+++ app/methods.js
@@ -2,6 +2,17 @@
 
 // Steam-Card-Bot-PRO built by Refloow (-MajokingGames)
 
+
+/*
+
+Want active support and updates with new features all for free?
+
+Leave an star on github repo its free ( we push updates based on the engagement )
+Repo link: https://github.com/OSL-Works/Steam-Card-Bot-PRO 
+
+*/
+
+
 /* 
   Here is contact info: refloowlibrarycontact@gmail.com
   or main dev steam: https://steamcommunity.com/id/MajokingGames/
@@ -338,6 +349,18 @@ t = module.exports = {
 }
 
 // Steam-Card-Bot-PRO built by Refloow (-MajokingGames)
+
+
+/*
+
+Want active support and updates with new features all for free?
+
+Leave an star on github repo its free ( we push updates based on the engagement )
+Repo link: https://github.com/OSL-Works/Steam-Card-Bot-PRO 
+
+*/
+
+
 
 /* 
   Here is contact info: refloowlibrarycontact@gmail.com
