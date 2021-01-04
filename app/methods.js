f100667916fd74935fcbe70bd7e5cedf01f9fb29
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2021-01-04 12:28:03 +0100

    Added notes

--- app/methods.js
+++ app/methods.js
@@ -1,24 +1,29 @@
 // We strongly recommend not editing stuff that is in this file.
 
 // Steam-Card-Bot-PRO built by Refloow (-MajokingGames)
-
-
+	
+	    
 /*
 
-Want active support and updates with new features all for free?
+Want active support and new updates with new features all for free?
 
 Leave an star on github repo its free ( we push updates based on the engagement )
-Discord Support Server: https://discord.gg/D8WCtDD     (Open an ticket)
 Repo link: https://github.com/OSL-Works/Steam-Card-Bot-PRO 
 
-*/
+  Discord Support Server: https://discord.gg/D8WCtDD     (Open an ticket)
 
+*/
+	
 
 /* 
   Here is contact info: refloowlibrarycontact@gmail.com
-  or main dev steam: https://steamcommunity.com/id/MajokingGames/
+  main dev steam: https://steamcommunity.com/id/MajokingGames/
+  
+  Donate: https://ko-fi.com/refloow
 
-*/
+ */
+
+
 
 try {
     // Checking if module colors is installed
@@ -350,22 +355,26 @@ t = module.exports = {
 }
 
 // Steam-Card-Bot-PRO built by Refloow (-MajokingGames)
-
-
+	
+	    
 /*
 
-Want active support and updates with new features all for free?
+Want active support and new updates with new features all for free?
 
 Leave an star on github repo its free ( we push updates based on the engagement )
 Repo link: https://github.com/OSL-Works/Steam-Card-Bot-PRO 
 
+  Discord Support Server: https://discord.gg/D8WCtDD     (Open an ticket)
+
 */
-
-
+	
 
 /* 
   Here is contact info: refloowlibrarycontact@gmail.com
-  Discord Support Server: https://discord.gg/D8WCtDD     (Open an ticket)
-  or main dev steam: https://steamcommunity.com/id/MajokingGames/
+  main dev steam: https://steamcommunity.com/id/MajokingGames/
+  
+  Donate: https://ko-fi.com/refloow
 
-*/
+ */
+
+
