aa068f2e49adaf1c74308b555c9451cc27825cd5
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2021-03-10 00:00:54 +0100

    Added notes

--- app/logcolors.js
+++ app/logcolors.js
@@ -87,5 +87,8 @@ const logger = module.exports = winston.createLogger({
   level: 'info'
 });
 
-// Original work: Copyright (c) 2020-2021 Refloow All rights reserved.
+// Copyright notice:
+
+/* Original work: Copyright (c) 2020-2021 Refloow All rights reserved.
+  Code origin (Free GitHub publish): https://github.com/OSL-Works/Steam-Card-Bot-PRO*/
 
