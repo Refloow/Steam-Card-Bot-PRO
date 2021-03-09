a240fe0218246d7ab37035e570e391af96aa4573
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2021-03-10 00:02:48 +0100

    Added notes

--- app/SETTINGS/[Visual Config lookup]/index.js
+++ app/SETTINGS/[Visual Config lookup]/index.js
@@ -1,4 +1,7 @@
-// Original work: Copyright (c) 2020-2021 Refloow All rights reserved.
+// Copyright notice:
+
+/* Original work: Copyright (c) 2020-2021 Refloow All rights reserved.
+  Code origin (Free GitHub publish): https://github.com/OSL-Works/Steam-Card-Bot-PRO*/
 
 const CCMethods = require('./app/ConfigCheckMethods.js');
 
