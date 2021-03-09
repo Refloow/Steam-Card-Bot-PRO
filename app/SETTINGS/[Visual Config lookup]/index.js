975d079c89b6770152a39a7825c30174d3d5fc08
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2021-03-09 22:55:25 +0100

    Added notes

--- app/SETTINGS/[Visual Config lookup]/index.js
+++ app/SETTINGS/[Visual Config lookup]/index.js
@@ -1,20 +1,5 @@
-// VISUAL CONFIG LOOKUP built by Refloow (-MajokingGames)
-
-/* 
-  Here is contact info: refloowlibrarycontact@gmail.com
-  or main dev steam: https://steamcommunity.com/id/MajokingGames/
-
-*/
+// Original work: Copyright (c) 2020-2021 Refloow All rights reserved.
 
 const CCMethods = require('./app/ConfigCheckMethods.js');
 
 CCMethods.validatelogininfo();
-
-
-// VISUAL CONFIG LOOKUP built by Refloow (-MajokingGames)
-
-/* 
-  Here is contact info: refloowlibrarycontact@gmail.com
-  or main dev steam: https://steamcommunity.com/id/MajokingGames/
-
-*/
