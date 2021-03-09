486c9758dfb793f4a977cf30dccd5922ae9e7f56
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2021-03-09 16:34:20 +0100

    Added notes

--- app/functions.js
+++ app/functions.js
@@ -653,27 +653,5 @@ function sortSetsByAmountB(SETS, callback) {
 
 module.exports=f;
 
-// Steam-Card-Bot-PRO built by Refloow (-MajokingGames)
-	
-	    
-/*
-
-Want active support and new updates with new features all for free?
-
-Leave an star on github repo its free ( we push updates based on the engagement )
-Repo link: https://github.com/OSL-Works/Steam-Card-Bot-PRO 
-
-  Discord Support Server: https://discord.gg/D8WCtDD     (Open an ticket)
-
-*/
-	
-
-/* 
-  Here is contact info: refloowlibrarycontact@gmail.com
-  main dev steam: https://steamcommunity.com/id/MajokingGames/
-  
-  Donate: https://ko-fi.com/refloow
-
- */
-
+// Original work: Copyright (c) 2021 Refloow All rights reserved.
 
