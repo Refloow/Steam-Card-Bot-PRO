98fc1ac35600cfd954003fffc5b9c3a4f75dd8ef
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2021-03-09 16:28:58 +0100

    Added notes

--- app/app.js
+++ app/app.js
@@ -1,27 +1,40 @@
-// Steam-Card-Bot-PRO built by Refloow (-MajokingGames)
-	
-	    
-/*
 
-Want active support and new updates with new features all for free?
+// Copyright notice:
 
-Leave an star on github repo its free ( we push updates based on the engagement )
-Repo link: https://github.com/OSL-Works/Steam-Card-Bot-PRO 
+/*--------------------------------------------------------------------------------------------- 
+* Original work: Copyright (c) 2021 Refloow All rights reserved.
 
-  Discord Support Server: https://discord.gg/D8WCtDD     (Open an ticket)
+* Code origin: https://github.com/OSL-Works/Steam-Card-Bot-PRO
+* Developer name: Veljko Vuckovic
+* Licensed under the MIT License. See LICENSE in the project root for license information.
+* Published License: https://github.com/OSL-Works/Steam-Card-Bot-PRO/master/LICENSE
+
+* Contact information:
+  Discord Support Server: https://discord.gg/D8WCtDD
+  Main developer steam: https://steamcommunity.com/id/MajokingGames/ 
+  Mail: refloowlibrarycontact@gmail.com
+
+* Donations:
+  Donate: https://ko-fi.com/refloow
+ --------------------------------------------------------------------------------------------*/
+
+ /* 
+
+// legal advice: PERMISSIONS AND RIGHTS
+
+* License does not prohibit modification, distribution, private/commercial use or sale of copies as long as the original LICENSE file
+ and authors copyright notice are left ass they are in the project files.
+* Copyright notice could be included ones or multiple times within the file.
+* Copyright notice should not be removed even within the larger works (Larger modifications applied).
+* Original file tags cannot be removed without creators exclusive permission.
+* Adding own tags in files is possible in case of modification - even in that case original tags must be kept.
+
+===================================================================================
+Removing copyright notice & distributing, using or selling the software without
+the original license and copyright notice is licence agreement breach and its considered criminal offense and piracy.
+===================================================================================
 
 */
-	
-
-/* 
-  Here is contact info: refloowlibrarycontact@gmail.com
-  main dev steam: https://steamcommunity.com/id/MajokingGames/
-  
-  Donate: https://ko-fi.com/refloow
-
- */
-
-
 
 
 // Checking if all modules are correctly installed
