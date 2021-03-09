429fae311328f0488215f430febb95fba7182474
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2021-03-09 22:49:41 +0100

    Added notes

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 // Copyright notice:
 
 /*--------------------------------------------------------------------------------------------- 
-* Original work: Copyright (c) 2021 Refloow All rights reserved.
+* Original work: Copyright (c) 2020-2021 Refloow All rights reserved.
 
 * Code origin: https://github.com/OSL-Works/Steam-Card-Bot-PRO
 * Developer name: Veljko Vuckovic
@@ -28,6 +28,10 @@
 * Copyright notice should not be removed even within the larger works (Larger modifications applied).
 * Original file tags cannot be removed without creators exclusive permission.
 * Adding own tags in files is possible in case of modification - even in that case original tags must be kept.
+* Year on the copyright notice breakdown:
+* Generally, the “year of first publication of the work” refers to the year in which the work was first distributed to the public (first year mentioned)
+* Any year after represents the year of added modifications.
+* Copyright cannot expire so therefore you cannot remove copyright notice if its not updated to the latest year.
 
 ===================================================================================
 Removing copyright notice & distributing, using or selling the software without
@@ -81,7 +85,7 @@ console.log('888   T88b "Y8888 888   888 "Y88P"  "Y88P"  "Y8888888P" \n\n'.cyan)
 console.log('▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀\n' .red);                             
 console.log('This bot was developed by MajokingGames [Dev] at - Refloow and published for free at OSL-Works GitHub org'.cyan);
 console.log('Code origin: https://github.com/OSL-Works/Steam-Card-Bot-PRO' .red);
-console.log('Original work: Copyright (c) 2021 Refloow All rights reserved.\n' .green);
+console.log('Original work: Copyright (c) 2020-2021 Refloow All rights reserved.\n' .green);
 console.log('▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀▀\n' .red);
 console.log('Removing copyright notice above & distributing, using or selling the software without \n the original license and notice above is licence agreement breach and its considered criminal offense.\n' .yellow);
 
