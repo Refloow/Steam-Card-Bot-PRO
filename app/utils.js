7e170cade7b8b534c8613b4ecb8dc9a8bf05a21a
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2021-03-10 00:00:31 +0100

    Added notes

--- app/utils.js
+++ app/utils.js
@@ -255,4 +255,4 @@ module.exports = t;
 // Copyright notice:
 
 /* Original work: Copyright (c) 2020-2021 Refloow All rights reserved.
-  Code origin: https://github.com/OSL-Works/Steam-Card-Bot-PRO */
+  Code origin (Free GitHub publish): https://github.com/OSL-Works/Steam-Card-Bot-PRO*/
