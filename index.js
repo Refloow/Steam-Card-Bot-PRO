8ed0c18bd072f0414d204d16f4317ae646b0babe
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2021-03-10 00:03:55 +0100

    Added notes

--- index.js
+++ index.js
@@ -149,5 +149,5 @@ mongoose.connect(url,connectionParams)
 // Copyright notice:
 
 /* Original work: Copyright (c) 2020-2021 Refloow All rights reserved.
-  Code origin: https://github.com/OSL-Works/Steam-Card-Bot-PRO */
+  Code origin (Free GitHub publish): https://github.com/OSL-Works/Steam-Card-Bot-PRO*/
 
