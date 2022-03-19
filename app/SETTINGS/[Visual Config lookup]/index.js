c6bdc7cb00034fa4d890636f01d50932affbf2c7
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2022-03-19 23:48:42 +0100

    ❌ Removing depreciated

--- app/SETTINGS/[Visual Config lookup]/index.js
+++
@@ -1,8 +0,0 @@
-// Copyright notice:
-
-/* Original work: Copyright (c) 2020-2021 Refloow All rights reserved.
-  Code origin (Free GitHub publish): https://github.com/OSL-Works/Steam-Card-Bot-PRO*/
-
-const CCMethods = require('./app/ConfigCheckMethods.js');
-
-CCMethods.validatelogininfo();
