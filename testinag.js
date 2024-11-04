99d2a39e996563745a3b6a7cac86918b899ddce0
Veljko Vuckovic <59449926+Refloow@users.noreply.github.com>
2024-11-04 18:37:42 +0100

    delete testing

--- testinag.js
+++
@@ -1,6 +0,0 @@
-        document.addEventListener('DOMContentLoaded', function() {
-            document.body.addEventListener('click', function() {
-                console.log("Body clicked!");
-                document.body.style.backgroundColor = 'red';
-            });
-        });
